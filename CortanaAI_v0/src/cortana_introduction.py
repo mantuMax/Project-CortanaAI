71eabc3b0bd6b4abfd30274e4224172fa3aeb8c9
Starboy <samskywalker001@gmail.com>
2022-04-25 16:51:40 +0530

    Project-CortanaAI_v0

---
+++ CortanaAI_v0/src/cortana_introduction.py
@@ -0,0 +1,24 @@
+# Project-CortanaAI, Level 0, Step-1
+
+
+# CortanaAI Introduction...
+
+from pyfiglet import figlet_format
+
+
+def introduction():
+    out = figlet_format("Cortana  AI", "doom")
+    return out
+
+
+print(introduction())
+
+
+"""
+# Resources:
+
+        >_ Web; http://www.figlet.org/examples.html
+
+        >_ Fonts; standard, big, mirror, script, doom
+
+"""
