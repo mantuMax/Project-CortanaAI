b5b9ad2997b5aa8bffebd2b3d8d221ab33bba38c
Starboy <samskywalker001@gmail.com>
2022-04-28 05:23:47 +0530

    Cortana Version-ZERO Finished

--- CortanaAI_v0/tests/testONE.py
+++ CortanaAI_v0/tests/testONE.py
@@ -1,2 +1,24 @@
-# Web Commands...
+# How to send requests to PowerShell rather than, cmd?
+
+# import os
+from os import system,name
+
+# os.system("dir")      #cmd
+
+
+# print(dir(os.system))
+
+print(system.__doc__)
+
+print(name)
+
+
+
+
+import datetime
+
+print(datetime.date.today())
+
+open("google.com")
+
 
