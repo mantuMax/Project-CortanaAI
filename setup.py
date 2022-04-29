1bb7159f399b45dad042e07e9191dad7618a96ab
Starboy <samskywalker001@gmail.com>
2022-04-29 18:58:01 +0530

    Release-vZERO

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CortanaAI',
+    version='0.0',
+    packages=[''],
+    url='https://github.com/skywalkerSam/CortanaAI',
+    license='MIT',
+    author='Sam Skywalker',
+    author_email='samskywalker.asaiinc@gmail.com',
+    description='This is the very first release of CortanaAI...'
+)
